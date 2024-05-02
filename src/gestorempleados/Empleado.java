6f8a8864a8b99ec411d4422365dfc5723bd024a7
Ana Pastor <apr0050@alu.medac.es>
2024-05-02 13:40:09 +0200

    Creada clase Departamento.

--- src/gestorempleados/Empleado.java
+++ src/gestorempleados/Empleado.java
@@ -7,12 +7,49 @@
  *
  * @author Usuario
  */
-public class Empleado {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        // TODO code application logic here
+class Empleado {
+    private String nombre;
+    private double salario;
+    private String departamento;
+
+    public Empleado(String nombre, double salario, String departamento) {
+        this.nombre = nombre;
+        this.salario = salario;
+        this.departamento = departamento;
+    }
+
+    // Getters y setters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public String getDepartamento() {
+        return departamento;
+    }
+
+    public void setDepartamento(String departamento) {
+        this.departamento = departamento;
+    }
+
+    @Override
+    public String toString() {
+        return "Empleado{" +
+                "nombre='" + nombre + '\'' +
+                ", salario=" + salario +
+                ", departamento='" + departamento + '\'' +
+                '}';
     }
 }
